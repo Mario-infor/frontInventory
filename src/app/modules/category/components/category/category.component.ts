5cab1ddc30ec4fa45c7f3b870b5c77c3e2459eb4
MarioDev <mherreraalmira@gmail.com>
2024-07-07 22:19:28 -0600

    Add put functionality for categories

--- src/app/modules/category/components/category/category.component.ts
+++ src/app/modules/category/components/category/category.component.ts
@@ -1,6 +1,9 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
 import { CategoryService } from 'src/app/modules/shared/services/category.service';
+import { NewCategoryComponent } from '../new-category/new-category.component';
+import { MatSnackBar, MatSnackBarRef, SimpleSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-category',
@@ -10,6 +13,9 @@ import { CategoryService } from 'src/app/modules/shared/services/category.servic
 export class CategoryComponent implements OnInit{
   
   private categoryService = inject(CategoryService);
+  private snackBar = inject(MatSnackBar);
+  public dialog = inject(MatDialog);
+
 
   ngOnInit(): void {
     this.getCategories();
@@ -42,6 +48,29 @@ export class CategoryComponent implements OnInit{
 
       this.dataSource = new MatTableDataSource<CategoryElement>(dataCategory);
     }
+  }
+
+  openCategoryDialog() {
+    const dialogRef = this.dialog.open( NewCategoryComponent, {
+      width: '450px'
+    });
+
+    dialogRef.afterClosed().subscribe((result: any) => {
+
+      if(result == 1){
+        this.openSnackBar("Categoría agregada", "Exitosa");
+        this.getCategories();
+
+      } else if(result == 2){
+        this.openSnackBar("Se produjo un error al guardar categoría", "Error");
+      }
+    });
+  }
+
+  openSnackBar(message: string, action: string) : MatSnackBarRef<SimpleSnackBar> {
+    return this.snackBar.open(message, action, {
+      duration: 2000
+    });
   }
 }
 
--- src/app/modules/shared/services/category.service.ts
+++ src/app/modules/shared/services/category.service.ts
@@ -17,4 +17,13 @@ export class CategoryService {
     const endpoint = `${base_url}/categories`;
     return this.http.get(endpoint);
   }
+
+  /**
+   * save the categories
+   */
+
+  saveCategory(body: any) {
+    const endpoint = `${base_url}/categories`;
+    return this.http.post(endpoint, body);
+  }
 }
